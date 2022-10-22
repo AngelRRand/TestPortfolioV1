014800f2816e49e15bfb74767216129b544ca579
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-10-22 20:51:46 -0300

    break

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -1,12 +1,12 @@
 import { useLocation } from "react-router-dom";
-
+import './Nav.css'
 const Nav = () => {
     const location = useLocation();
 
 
     if(location.pathname.toString() !== '/'){
         return (
-            <div>Nav</div>
+            <nav className="nav">Nav</nav>
         )  
     }else{
         return <></>
