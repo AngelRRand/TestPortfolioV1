f1cda99ae5f89303faa368887c48e6424679ea22
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-10-22 20:12:32 -0300

    fixed

--- src/view/landingpage/LandingPage.tsx
+++ src/view/landingpage/LandingPage.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
 import { Link } from "react-router-dom";
 import Container from '../../component/Container';
 import Stars from '../../component/Stars';
 const LandingPage = () => {
   return (
     <Container styles='container_landingPage'>
+      <section className="landingPage">
+
       <Stars/>
       <Link to={'/home'}>Home</Link>
+      </section>
     </Container>
   )
 }
