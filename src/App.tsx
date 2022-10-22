b5a3fad958c9fa3e5f32a4d598035ebec5db4c9e
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-10-21 23:05:18 -0300

    new Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { useLocation, Route, Routes } from "react-router-dom";
 import { AnimatePresence } from 'framer-motion';
-import './App.css'
 import Home from "./view/Home";
+import LandingPage from "./view/landingpage/LandingPage";
+import Nav from "./component/Nav";
+
+import './App.css'
 function App() {
   const location = useLocation();
   return (
     <>
       <AnimatePresence>
         <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </AnimatePresence>
+      <Nav/>
     </>
   );
 }
--- src/view/Home.tsx
+++ src/view/Home.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Container from '../component/Container'
 import './Home.css'
 const Home = () => {
