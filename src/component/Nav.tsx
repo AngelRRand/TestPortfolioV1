d71fa548fbe079f725c299a11cc5e443c557f19b
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-10-23 21:54:07 -0300

    add images

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -1,5 +1,5 @@
 import logo from '../assets/NavBotton/PJprueba.gif'
-import logoSinCasco from '.../assets/NavBotton/PJpruebaSinCasco.gif'
+import logoSinCasco from '../assets/NavBotton/PJpruebaSinCasco.gif'
 
 
 import { useLocation, useNavigate } from "react-router-dom";
@@ -8,12 +8,15 @@ import './Nav.css'
 const Nav = () => {
     const location = useLocation();
 
-
     if(location.pathname.toString() !== '/'){
         return (
             <nav className="nav">
                 <div className='container_perfil'>
-                    <img src={logo} alt="" className='img_perfil'/>
+                    <img 
+                        src={location.pathname.toString() === '/about/' ? logoSinCasco : logo} 
+                        alt="" 
+                        className='img_perfil'/>
+                    <h2>Orasio</h2>
                 </div>
             </nav>
         )  
