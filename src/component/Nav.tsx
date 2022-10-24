92aabbc99fd55ba5d8af057468f38662584ae218
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-10-23 21:59:07 -0300

    fixed error

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -1,5 +1,7 @@
 import logo from '../assets/NavBotton/PJprueba.gif'
+import nutria from '../assets/NavBotton/nutria.gif'
 import logoSinCasco from '../assets/NavBotton/PJpruebaSinCasco.gif'
+import nutriaSinCasco from '../assets/NavBotton/nutriaSinCasco.gif'
 
 
 import { useLocation, useNavigate } from "react-router-dom";
@@ -11,11 +13,21 @@ const Nav = () => {
     if(location.pathname.toString() !== '/'){
         return (
             <nav className="nav">
-                <div className='container_perfil'>
+                <div className='container_perfil left_perfil'>
                     <img 
                         src={location.pathname.toString() === '/about/' ? logoSinCasco : logo} 
                         alt="" 
-                        className='img_perfil'/>
+                        className='img_perfil '/>
+                    <h2>Orasio</h2>
+                </div>
+
+                h2
+
+                <div className='container_perfil rigth_perfil'>
+                    <img 
+                        src={location.pathname.toString() === '/about/' ? nutriaSinCasco : nutria} 
+                        alt="" 
+                        className='img_perfil '/>
                     <h2>Orasio</h2>
                 </div>
             </nav>
