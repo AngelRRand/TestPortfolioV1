0b4489ca1cdfeb117d0621dbdf26348f3942f93b
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-10-25 20:27:48 -0300

    templet planet add

--- src/view/education/Education.tsx
+++ src/view/education/Education.tsx
@@ -4,10 +4,11 @@ import Planet from '../../assets/Planet/PlanetZOOM.png'
 import Señal from '../../assets/NavImg/Señal.gif'
 import PlanetTemplet from '../../component/PlanetTemplet';
 
-
 import { motion } from "framer-motion";
 import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
+
+import './Education.css'
 const Education = () => {
 
   const navigate = useNavigate()
@@ -16,7 +17,26 @@ const Education = () => {
 
   return (
     <Container styles='container_planet'>
-<Stars />
+      <Stars />
+      <motion.main
+        className='education'
+        initial={{ scale: 0, x: -3000 }}
+        animate={{
+          scale: 1,
+          x: 0,
+          transition: { duration: 1.5, type: "spring", },
+        }}
+      >
+        <PlanetTemplet
+          show={show}
+          animation={animation}
+          name={'About'}
+          planet={Planet}
+          transitionStyle={'home_transition'}
+        >
+
+        </PlanetTemplet>
+      </motion.main>
     </Container>
   )
 }
