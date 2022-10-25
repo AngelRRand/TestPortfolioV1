7ef61567c07b11d1c63af04b26a5c4068d16531e
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-10-24 21:37:14 -0300

    states

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -3,36 +3,43 @@ import nutria from '../assets/NavBotton/nutria.gif'
 import logoSinCasco from '../assets/NavBotton/PJpruebaSinCasco.gif'
 import nutriaSinCasco from '../assets/NavBotton/nutriaSinCasco.gif'
 
-
+import Typewriter from 'typewriter-effect';
 import { useLocation, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import './Nav.css'
 const Nav = () => {
     const location = useLocation();
 
-    if(location.pathname.toString() !== '/'){
+    if (location.pathname.toString() !== '/') {
         return (
             <nav className="nav">
                 <div className='container_perfil left_perfil'>
-                    <img 
-                        src={location.pathname.toString() === '/about/' ? logoSinCasco : logo} 
-                        alt="" 
-                        className='img_perfil '/>
+                    <img
+                        src={location.pathname.toString() === '/about/' ? logoSinCasco : logo}
+                        alt=""
+                        className='img_perfil ' />
                     <h2>Orasio</h2>
                 </div>
 
-                h2
+                <Typewriter
+                    onInit={(typewriter) => {
+                        typewriter.typeString('You are at home, you can visit the other views of the page following the map or cheating if you are a developer ;)')
+                            .pauseFor(1500)
+                            .deleteAll()
+                            .start();
+                    }}
+                />
 
                 <div className='container_perfil rigth_perfil'>
-                    <img 
-                        src={location.pathname.toString() === '/about/' ? nutriaSinCasco : nutria} 
-                        alt="" 
-                        className='img_perfil '/>
+                    <img
+                        src={location.pathname.toString() === '/about/' ? nutriaSinCasco : nutria}
+                        alt=""
+                        className='img_perfil ' />
                     <h2>Orasio</h2>
                 </div>
             </nav>
-        )  
-    }else{
+        )
+    } else {
         return <></>
     }
 }
--- src/view/about/About.tsx
+++ src/view/about/About.tsx
@@ -1,7 +1,31 @@
 import Container from '../../component/Container'
 import Stars from '../../component/Stars'
 
+var transitions = {
+  me: {
+    scale: 7,
+    x: 350,
+    y: -1150,
+    transition: { duration: 6.5, type: "spring", bounce: 0 },
+  },
+  dream: {
+    scale: 7,
+    x: -500,
+    y: 1100,
+    transition: { duration: 6.5, type: "spring", bounce: 0 },
+  },
+  art: {
+    scale: 7,
+    x: 750,
+    y: -450,
+    transition: { duration: 6.5, type: "spring", bounce: 0 },
+  }
+}
+
 const About = () => {
+
+
+
   return (
     <Container styles='container_planet'>
       <Stars />
