11b7eab244177a60ec0b201bb2a1a988ca7117e5
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-10-25 20:22:26 -0300

    import components education

--- src/view/about/About.tsx
+++ src/view/about/About.tsx
@@ -3,11 +3,11 @@ import Stars from '../../component/Stars'
 import Planet from '../../assets/Planet/PlanetZOOM.png'
 import Señal from '../../assets/NavImg/Señal.gif'
 
+import PlanetTemplet from '../../component/PlanetTemplet';
 import { motion } from "framer-motion";
 import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import './About.css'
-import PlanetTemplet from '../../component/PlanetTemplet';
 
 var transitions = {
   house: {
--- src/view/education/Education.tsx
+++ src/view/education/Education.tsx
@@ -1,5 +1,13 @@
-import React from 'react'
+import Container from '../../component/Container'
+import Stars from '../../component/Stars'
+import Planet from '../../assets/Planet/PlanetZOOM.png'
+import Señal from '../../assets/NavImg/Señal.gif'
+import PlanetTemplet from '../../component/PlanetTemplet';
 
+
+import { motion } from "framer-motion";
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 const Education = () => {
   return (
     <div>Education</div>
