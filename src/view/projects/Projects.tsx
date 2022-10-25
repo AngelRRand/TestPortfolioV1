4d0505cb954f6341c6d17fbfd7418fb2dc40ea32
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-10-25 20:57:54 -0300

    import projects

--- src/view/projects/Projects.tsx
+++ src/view/projects/Projects.tsx
@@ -1,5 +1,14 @@
-import React from 'react'
+import Container from '../../component/Container'
+import Stars from '../../component/Stars'
+import Planet from '../../assets/Planet/IcePlanetZOOM.png'
+import Señal from '../../assets/NavImg/Señal.gif'
+import PlanetTemplet from '../../component/PlanetTemplet';
 
+import { motion } from "framer-motion";
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
+import './Projects.css'
 const Projects = () => {
   return (
     <div>Projects</div>
