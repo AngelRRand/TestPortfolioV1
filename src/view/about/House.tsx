e26c7da1999a5f41441c7ae39e2e1fa8d4009199
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-10-28 19:11:56 -0300

    grid styles cat

--- src/view/about/House.tsx
+++ src/view/about/House.tsx
@@ -3,8 +3,6 @@ import HouseImg from '../../assets/Planet/Terra/House/House.gif'
 import HouseImgGif from '../../assets/Planet/Terra/House/HouseFlora.gif'
 import NubeBot from '../../assets/nubes.png'
 import NubesTop from '../../assets/nubesTop.png'
-import lightAuto from '../../assets/Planet/Terra/House/LuzHover.gif'
-import lightHover from '../../assets/Planet/Terra/House/LuzHover.gif'
 import lightEffect from '../../assets/Planet/Terra/House/LuzEffectHover.gif'
 const House = () => {
 
