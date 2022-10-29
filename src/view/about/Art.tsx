a5894b7e602115979c04acf73a6caa3ee10ac462
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-10-28 23:08:47 -0300

    fixed styles

--- src/view/about/Art.tsx
+++ src/view/about/Art.tsx
@@ -3,6 +3,10 @@ import ArtImg from '../../assets/Planet/Terra/Art/ArtBackground.png'
 import ArtGif from '../../assets/Planet/Terra/Art/ArtPajaros.gif'
 import NubeBot from '../../assets/nubes.png'
 import NubesTop from '../../assets/nubesTop.png'
+import Brightness from '../../assets/Planet/Terra/Art/brightness.gif'
+import Happydog from '../../assets/Planet/Terra/Art/guau_guau.png'
+
+import './Art.css'
 const Art = () => {
   return (
     <Wallpaper
@@ -12,7 +16,32 @@ const Art = () => {
       nubetop={NubesTop}
       nube_bg={'nube_terra_bg'}
     >
+      <div className='view_grid'>
+        <div className='view_item view_planet'>
+          <img className='view_object' src={Brightness} alt="ligth" />
+          <div className='view_card'>
+            <h2>Planet</h2>
+            <p>In front of a window I find myself in constant contact with the sunlight.</p>
+          </div>
+        </div>
 
+        <div className='view_item view_happyDog'>
+          <img className='view_object' src={Brightness} alt="cat body" />
+          <div className='view_card'>
+            <h2>The happy dog</h2>
+            <p>His name is twenty-seven, he is always at my house :D</p>
+            <img className='view_card_img' src={Happydog} alt="" />
+          </div>
+        </div>
+
+        <div className='view_item view_catSleep'>
+          <img className='view_object' src={Brightness} alt="cat body" />
+          <div className='view_card'>
+            <h2>Sleep cat</h2>
+            <p>His name is twenty-seven, he is always at my house :D</p>
+          </div>
+        </div>
+      </div>
     </Wallpaper>
   )
 }
