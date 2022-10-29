f85ab3fa14558a397571e27fee11c16d582834f6
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-10-28 22:23:22 -0300

    import dream

--- src/view/about/Dream.tsx
+++ src/view/about/Dream.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
-
+import Wallpaper from '../../component/Wallpaper'
+import ArtImg from '../../assets/Planet/Terra/Art/ArtBackground.png'
+import ArtGif from '../../assets/Planet/Terra/Art/ArtPajaros.gif'
+import NubeBot from '../../assets/nubes.png'
+import NubesTop from '../../assets/nubesTop.png'
 const Dream = () => {
   return (
-    <div>Dream</div>
+    <Wallpaper
+      image={ArtImg}
+      imageGif={ArtGif}
+      nubebot={NubeBot}
+      nubetop={NubesTop}
+      nube_bg={'nube_terra_bg'}
+    >
+      
+    </Wallpaper>
   )
 }
 
