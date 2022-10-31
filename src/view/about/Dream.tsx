4b3f12e0ffe1b2ee7c2d313cfbf606125386ef2a
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-10-31 15:45:01 -0300

    update image

--- src/view/about/Dream.tsx
+++ src/view/about/Dream.tsx
@@ -1,6 +1,6 @@
 import Wallpaper from '../../component/Wallpaper'
-import ArtImg from '../../assets/Planet/Terra/Art/ArtBackground.png'
-import ArtGif from '../../assets/Planet/Terra/Art/ArtPajaros.gif'
+import ArtImg from '../../assets/Planet/Terra/Dream/Moon.png'
+import ArtGif from '../../assets/Planet/Terra/Dream/Moon.gif'
 import NubeBot from '../../assets/nubes.png'
 import NubesTop from '../../assets/nubesTop.png'
 const Dream = () => {
