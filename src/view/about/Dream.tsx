1a37316e0b1ab8e2de3ddf84ae9b93fd461703c8
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-10-31 22:30:26 -0300

    test

--- src/view/about/Dream.tsx
+++ src/view/about/Dream.tsx
@@ -37,7 +37,7 @@ const Dream = () => {
                 autoStart: true,
                 loop: false,
                 delay: 150,
-                wrapperClassName: 'text_typing',
+                wrapperClassName: 'dream',
               }}
             />
           </div>
