375d282ed3d013158345c0c8584a0ef62b01acb3
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-01 18:54:34 -0300

    styles btn dream

--- src/view/about/Dream.tsx
+++ src/view/about/Dream.tsx
@@ -76,8 +76,10 @@ const Dream = () => {
               > My ambitions and goals are the motivation that drive me every day beyond my limits. </motion.p>
           </div>
         </motion.div>
-        <div className='btn_dream'>
-            <h1>BOTM</h1>
+        <div className='btn_container_dream'>
+          <button className='btn_dream pointer'>
+            <p>Up</p>
+          </button>
           </div>
       </Wallpaper>
 
