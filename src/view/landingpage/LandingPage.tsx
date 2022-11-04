8e2445fe6cdc72d221ae648034438d95c92830f7
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-04 13:24:43 -0300

    styles pj

--- src/view/landingpage/LandingPage.tsx
+++ src/view/landingpage/LandingPage.tsx
@@ -1,6 +1,14 @@
-import { Link } from "react-router-dom";
+import Cuerpo from '../../assets/CreatePJ/HumanoTrajeChi.gif'
+import Rostro from '../../assets/CreatePJ/HumanoCara.gif'
+import Pelo from '../../assets/CreatePJ/HumanoCabelloNegro.gif'
+import Ojos from '../../assets/CreatePJ/HumanoOjoMarrones.gif'
 import Container from '../../component/Container';
 import Stars from "../../component/Stars";
+
+
+import { Link } from "react-router-dom";
+
+
 import './LandingPage.css'
 const LandingPage = () => {
   return (
@@ -11,7 +19,10 @@ const LandingPage = () => {
         <div>
           <h2>PJ</h2>
           <div className="landingPage_cont_pj">
-
+            <img className='cuerpo_pj' src={Cuerpo} alt="" />
+            <img className='rostro_pj' src={Rostro} alt="" />
+            <img className='pelo_pj' src={Pelo} alt="" />
+            <img className='ojos_pj' src={Ojos} alt="" />
           </div>
         </div>
       </section>
