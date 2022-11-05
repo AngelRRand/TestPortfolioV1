5530f141fa9aa37c9529d8038601258597045b24
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-05 17:59:14 -0300

    add reducer avatar

---
+++ src/context/AvatarReducer.ts
@@ -0,0 +1,12 @@
+import { Avatar } from "../interfaces/Interface"
+
+type AvatarActions = 
+    | { type: 'addAvatar', payload:Avatar  }
+
+
+
+export const avatarReducer = (state:Avatar, action:AvatarActions) => {
+    switch(key){
+        case
+    }
+}
