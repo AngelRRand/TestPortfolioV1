b0486e34cd0a55941946b51c3917123888c260ed
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-05 18:10:04 -0300

    break

--- src/context/AvatarProvider.tsx
+++ src/context/AvatarProvider.tsx
@@ -2,24 +2,22 @@ import { Avatar } from "../interfaces/Interface"
 import { AvatarContext } from "./AvatarContext"
 import { cosmeticDefault } from "../helper/images"
 const initial_state: Avatar = {
-    avatar: {
-        Nacionality: 'ARG',
-        Flag: cosmeticDefault.Arg,
-        Skin: cosmeticDefault.SkinB,
-        Hair: cosmeticDefault.BlackHair,
-        Eyes: cosmeticDefault.BrownEyes
-    }
+    nacionality: 'ARG',
+    flag: cosmeticDefault.Arg,
+    skin: cosmeticDefault.SkinB,
+    hair: cosmeticDefault.BlackHair,
+    eyes: cosmeticDefault.BrownEyes
 }
 
 
 interface props {
-    children:JSX.Element | JSX.Element[]
+    children: JSX.Element | JSX.Element[]
 }
 
-export const AvatarProvider = ({ children }:props) => {
-  return (
-    <AvatarContext.Provider value={{}}>
-        {children}
-    </AvatarContext.Provider>
-  )
+export const AvatarProvider = ({ children }: props) => {
+    return (
+        <AvatarContext.Provider value={{}}>
+            {children}
+        </AvatarContext.Provider>
+    )
 }
