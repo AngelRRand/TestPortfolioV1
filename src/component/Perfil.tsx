52dbfb5ca35852d052063dfa8d9a327f6967e254
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-05 21:40:03 -0300

    break

--- src/component/Perfil.tsx
+++ src/component/Perfil.tsx
@@ -1,15 +1,28 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useContext } from 'react';
 import { AvatarContext } from '../context/AvatarContext';
+import { cosmeticDefaultGif } from '../helper/imagesGif';
 const Perfil = () => {
 
     const { avatarState } = useContext(AvatarContext)
-    const [avatar, setAvatar] = useState({})
+    const [avatar, setAvatar] = useState({
+        nacionality: '',
+        flag: '',
+        skin: '',
+        hair: '',
+        eyes: ''
+    })
 
     useEffect(() => {
         setAvatar(avatarState)
         if (avatarState.nacionality === 'default') {
-            console.log(avatar)
+            setAvatar({
+                nacionality: 'default',
+                flag: cosmeticDefaultGif.Arg,
+                skin: cosmeticDefaultGif.SkinB,
+                hair: cosmeticDefaultGif.BlackHair,
+                eyes: cosmeticDefaultGif.BrownEyes
+            })
         }
     }, [avatar])
 
@@ -18,10 +31,10 @@ const Perfil = () => {
     return (
         <div className='container_perfil left_perfil'>
             <div className="avatar_container">
-                <img className='cuerpo_pj' src={avatarState.flag} alt="" />
-                <img className='rostro_pj' src={avatarState.skin} alt="" />
-                <img className='pelo_pj' src={avatarState.hair} alt="" />
-                <img className='ojos_pj' src={avatarState.eyes} alt="" />
+                <img className='cuerpo_pj' src={avatar.flag} alt="" />
+                <img className='rostro_pj' src={avatar.skin} alt="" />
+                <img className='pelo_pj' src={avatar.hair} alt="" />
+                <img className='ojos_pj' src={avatar.eyes} alt="" />
             </div>
             <h2>Horacio</h2>
         </div>
