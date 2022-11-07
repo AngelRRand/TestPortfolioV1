80f087b5faf4d1e81f50f5ee6fa47fc3feb7f733
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-11-07 17:44:34 -0300

    redux

--- src/context/AvatarContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import { Avatar } from "../interfaces/Interface";
-
-export type AvatarContextProps = {
-    avatarState: Avatar,
-    newAvatar: (avatar: Avatar) => void
-}
-
-export const AvatarContext = createContext<AvatarContextProps>({} as AvatarContextProps)
